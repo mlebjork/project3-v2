6d4067a1369cc38c525bd67f0d7cfda8208f7240
Unknown <mlestraus@gmail.com>
2019-02-04 20:38:32 -0700

    get and post

--- server/routes/peaks.js
+++ server/routes/peaks.js
@@ -1,30 +1,53 @@
 const express = require('express')
 const router = express.Router()
-// const User = require('../database/models/user')
+const Peak = require('../database/models/peaks')
 const passport = require('../passport')
 
 router.get('/', (req, res, next) => {
     console.log('===== peaks!!======')
-    res.send([
-        {
-            "name":"one",
-            height:"3000"
-        },
-        {
-            "name":"two",
-            height:"3100"
-        },
-        {
-            "name":"three",
-            height:"3330"
-        },
-    ])
-
+    // res.send([
+    //     {
+    //         "name":"one",
+    //         height:"3000"
+    //     },
+    //     {
+    //         "name":"two",
+    //         height:"3100"
+    //     },
+    //     {
+    //         "name":"three",
+    //         height:"3330"
+    //     },
+    // ])
+    Peak.find({}, function (err, peaks) {
+        console.log(peaks)
+        res.send(peaks)
+    });
 })
 
 router.post('/', (req, res) => {
-    const { name, height } = req.body
-    res.send(name)
+    const { name, height, user } = req.body
+    // ADD VALIDATION
+    // Peak.findOne({ username: username }, (err, user) => {
+    //     if (err) {
+    //         console.log('User.js post error: ', err)
+    //     } else if (user) {
+    //         res.json({
+    //             error: `Sorry, already a user with the username: ${username}`
+    //         })
+    //     }
+    //     else {
+            const newPeak = new Peak(        {
+                "name": Math.random(),
+                height: Math.random(),
+                user: 'one'
+            },)
+            newPeak.save((err, savedUser) => {
+                if (err) return res.json(err)
+                return res.json(savedUser)
+            })
+        // }
+    // })
 
 })
 
